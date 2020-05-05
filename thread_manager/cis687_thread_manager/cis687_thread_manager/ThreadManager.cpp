62831793fed8bccef171d2859f39c6b795608ada
James <james.elias95@gmail.com>
2020-05-04 23:00:12 -0700

    Update Thread Manager
    * Updated polling function in thread manager
    * Added functions to get current time for log messages
    * Added tracking for the number of threads available to run processes at any time

--- thread_manager/cis687_thread_manager/cis687_thread_manager/ThreadManager.cpp
+++ thread_manager/cis687_thread_manager/cis687_thread_manager/ThreadManager.cpp
@@ -2,50 +2,102 @@
 #include <thread>
 #include <mutex>
 #include <iostream>
+#include <string>
+#include <time.h>
+#include <chrono>
 
 using std::thread;
-using std::lock_guard;
+using std::unique_lock;
 using std::mutex;
 using std::cout;
 using std::endl;
+using std::string;
+using std::chrono::system_clock;
 
+// Base constructor - creates a number of threads equal to the total number supported by user's CPU minus one
 ThreadManager::ThreadManager()
-	: tasks(), taskMutex(), taskAvailable(), stop(false) {
+	: taskMutex(), taskAvailable(), stop(false) {
 	int numThreads = (thread::hardware_concurrency() > 2)
 		? thread::hardware_concurrency() - 1
 		: 1;
-
+	setNumThreadsAvailable(numThreads);
 	for (auto i = 0; i < numThreads; i++) {
 		availableThreads.push_back(thread());
 	}
 };
 
-void ThreadManager::pollForTasks() {
-	/*
-	while(!stop) {
-		unique_lock<mutex> lock(taskMutex);
-		taskAvailable.wait(taskAvailable.notify_one);
-		function<void()> task = tasks.pop();
-		try {
-			task();
-		} catch (std::string e) {
-			throw e;
-		}
+// Overload constructor - creates the specified numbers of threads
+ThreadManager::ThreadManager(int numThreads)
+	: taskMutex(), taskAvailable(), stop(false) {
+	setNumThreadsAvailable(numThreads);
+	for (auto i = 0; i < numThreads; i++) {
+		availableThreads.push_back(thread());
 	}
-	*/
+}
 
-	// I think you would initialize the thread calling an infinite loop to poll for tasks
-	// Use mutex to lock the thread and condition_variable to wake it up when there's a new task
+// Infinite loop function to check for available tasks in the queue and execute them
+// Returns the start and stop time as well as the task result (pass/fail)
+void ThreadManager::pollForTasks() {
 	for (auto& thr : availableThreads) {
-		thr = thread([]() {
-				cout << "hello" << endl;
-			}
-		);
+		thr = thread([this]() {
+			getResult();
+		});
+	}
+
+	while (true) {
+		numThreadsAvailable++;
+		unique_lock<mutex> lock(taskMutex);
+		taskAvailable.wait(lock, [this]() { return stop; });
+		numThreadsAvailable--;
+		string testResult = "";
+		try {
+			testResult = "Test starting: " + getTime();
+			// Run the function
+			testResult = testResult + "Test Passed! \n";
+		}
+		catch (string e) {
+
+			cout << "FAIL" << endl;
+		}
+		testResult = testResult + "Test ending: " + getTime();
+		cout << testResult << endl;
 	}
 };
 
+// Destructor for the thread manager - ensures all mutexes and threads are safely destroyed
 ThreadManager::~ThreadManager() {
+	unique_lock<mutex> lock(taskMutex);
+	stop = true;
+	lock.unlock();
+	taskAvailable.notify_all;
 	for (auto& thr : availableThreads) {
 		thr.join();
 	}
 };
+
+// Function that returns the number of threads available at any given time
+int ThreadManager::getNumThreadsAvailable() {
+	return numThreadsAvailable;
+};
+
+// Function that sets the number of threads available
+void ThreadManager::setNumThreadsAvailable(int threads) {
+	numThreadsAvailable = threads;
+};
+
+string ThreadManager::getResult() {
+	return "weee";
+};
+
+void ThreadManager::startThread(function<bool()> func) {
+
+};
+
+// Private helper function to get the time. Used to determine the start and stop time to be displayed
+string ThreadManager::getTime() {
+	auto rawTime = system_clock::now();
+	const time_t convertedTime = system_clock::to_time_t(rawTime);
+	char displayTime[30];
+	ctime_s(displayTime, sizeof displayTime, &convertedTime);
+	return string(displayTime);
+}
--- thread_manager/cis687_thread_manager/cis687_thread_manager/ThreadManager.h
+++ thread_manager/cis687_thread_manager/cis687_thread_manager/ThreadManager.h
@@ -6,24 +6,35 @@
 #include <functional>
 #include <mutex>
 #include <condition_variable>
+#include <string>
 
 using std::vector;
 using std::thread;
 using std::function;
 using std::mutex;
 using std::condition_variable;
+using std::string;
 
 class ThreadManager {
 	public:
 		ThreadManager();
+		ThreadManager(int numThreads);
 		~ThreadManager();
 		void pollForTasks();
+		int getNumThreadsAvailable();
+		void setNumThreadsAvailable(int threads);
+		string getResult();
+		void startThread(function<bool()> func);
 	private:
+		// Private helper functions
+		string getTime();
+
+		// Private data members
 		vector<thread> availableThreads;
-		vector<function<void()>> tasks;
 		mutex taskMutex;
 		condition_variable taskAvailable;
 		bool stop;
+		int numThreadsAvailable;
 };
 
 #endif
