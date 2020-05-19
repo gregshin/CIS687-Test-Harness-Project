65a47f162405b41801cfd1fc17225f7534ca9504
mark edward <markebusm@gmail.com>
2020-05-18 19:36:26 -0700

    Added project structure

---
+++ CSE687_Project/ThreadManager.h
@@ -0,0 +1,65 @@
+#ifndef THREAD_MANAGER_H
+
+#define THREAD_MANAGER_H
+
+
+
+#include <string>
+
+#include <thread>
+
+
+
+using std::string;
+
+using std::thread;
+
+
+
+class ThreadManager {
+
+public:
+
+	ThreadManager();
+
+	ThreadManager(int threads);
+
+	~ThreadManager();
+
+	string startThread(string dllLocation);
+
+	void setMaxThreads();
+
+	void setMaxThreads(int threads);
+
+	int getMaxThreads();
+
+	void setRunningThreads();
+
+	int getRunningThreads();
+
+	bool isThreadAvailable();
+
+
+
+	void sampleFunc();
+
+private:
+
+	// Private helper functions
+
+	string getTime();
+
+
+
+	// Private data members
+
+	int maxThreads;
+
+	int runningThreads;
+
+};
+
+
+
+#endif
--- Mark Input Queue/Source.cpp
+++ Mark Input Queue/Source.cpp
@@ -7,47 +7,18 @@
 #include "InputQueue.h"
 #include <iostream>
 
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-
 int main() {
-	
 
-	int size=10;
-	InputQueue IQ(size); //object constructor queue size 50 string elements
-	
 
-	//GUI sends input queue a int with the number of string file paths coming per test request
-	//ex: sent int 3 then sends 3 strings with a file path ex:"C:/Test/Dll/Test1"
+	InputQueue IQ(50);
 
-	//add to queue
-	IQ.enqueue("C:/Test/Dll/Test1");
-	IQ.enqueue("C:/Test/Dll/Test2");
-	IQ.enqueue("C:/Test/Dll/Test3");
+	IQ.enqueue("The");
+	IQ.enqueue("cat");
+	IQ.enqueue("is");
+	IQ.enqueue("fat");
 
-	//remove from queue and put into local variables for display
-	string x = IQ.dequeue();
-	string y = IQ.dequeue();
-	string z = IQ.dequeue();
-	
 
-	cout << "x= " << x << " ,y= " << y << " ,z= "<<z<<endl;
 
-	
-	int c = IQ.getcount();//get amount of elements currently in queue
-
-	cout << "Count is: " << c<<endl;
-
-	
-
-	
-	
-	
-
-	
 
 	return 0;
 }
